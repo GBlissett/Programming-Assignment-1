7e19cbf722724efdf5fdcaac010396bdc2ad7324
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-02 04:22:00 +0000

    More advanced code written that: - allows user to input letters (in the sequence of a message) and stores it in a string (ie forming a word of index w) - isolate each individual character in a string and compare it (in a loop) to letters of the alphabet - print encrypted word

--- Rotation_Cipher_Test.c
+++ Rotation_Cipher_Test.c
@@ -3,8 +3,8 @@
 //Encryption:
 int main(void)
 {
-   int k, i, letters[25], over, orig_letters[25], counter = 0;;
-   char encrypt_letter, orig_letter, text[1024], text_letter;
+   int k, i, letters[25], over, orig_letters[25], counter = 0, I, w;
+   char encrypt_letter, orig_letter, text[1024], orig_word[1024];
    
    printf("Enter offset of rotation cipher: \n");
    scanf("%d", &k);
@@ -28,25 +28,30 @@ int main(void)
        
        printf("%c now %c\n", orig_letter, encrypt_letter);       
    }
+   printf("Enter text to encrypt below: (NB make sure all text is in capital letters and separate each word by pressing the return key)\n");
    
- 
-   printf("Enter text to encrypt below: (NB make sure all text is in capital letters)\n");
-   while (counter < 1025)
+   for (w = 1; w < 1025; w++) //specifies what word is being encrypted
    {
-     scanf("%c", text_letter);
-     text[counter] = text_letter;
+    
+     scanf("%s", text); //creates a string out of letters input by user 
+    
+     for (I = 0; I < 1024; I++)
+     {  
+        i = 0;
+        
+        while (i < 26)
+        {
+             if (text[I] = orig_letters[i])
+             {
+                 text[I] = (char)letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
+             }
+             i++;
+        }
+     }
      
-     counter++;
+     printf("New Word: %s\n", text); //prints new word when computated DOES NOT WORK YET!!!!
      
-     printf("&d text counter is %c", counter, text_letter);
-   }
-   
-   if (counter == 1024)
-   {
-       printf("Maximum message length reached");
    }
    
 }
-
-
 //Decryption:
