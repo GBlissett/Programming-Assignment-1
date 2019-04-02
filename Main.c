8c0d2dbf9c81754eeb7fea87ce37a8eee389e7ff
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-02 00:39:30 +0000

    Declares prototypes for functions that will respectively produces: - encryption given an algorithm, message text, and key - decryption given an algorithm, cipher text, and key - decryption given cipher text and some assumptions of its contents without the key

--- Main.c
+++ Main.c
@@ -1,7 +1,9 @@
 #inlcude <stdio.h>
 
+char Encryp_alg_messtxt_key(void); //prototype for a function that perfoms encryption given an algorithm, message text, and key
+char Decryp_alg_ciph_txt_key(void); //prototype for a function that performs decryption given an algorithm, cipher text, and key
+char Decryp_ciph_txt_no_key(void); // prototype for a function that performs decryption given cipher text and some assumptions of its contentswithoutthe key
 int main ()
 {
-
 return 0;
 }
