430d8e8b2bc474cfb1d3a725243fbcc556914421
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-02 01:33:25 +0000

    Menus created for each function which allows user to specify if they are computing a rotation or a substitution cipher. The format/workings or each menu is the same as original menu (ie in Main.c).

--- Decryp_alg_ciph_txt_key.c
+++ Decryp_alg_ciph_txt_key.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("hello, world\n");
+// prototype for a function that performs decryption given cipher text and some assumptions of its contents without the key
+
+char cipher;
+    int int_cipher;
+    
+    // Menu that allows user to select type of cipher (NB Menu structure/workings is same as origional menu (ie in MAin.c)) 
+    printf("Select type of cipher: \n");
+    printf("    a: Rotation cipher\n");
+    printf("    b: Substitution cipher\n\n");
+    printf("Selection: "); 
+    scanf("%c", &cipher);
+    
+    int_cipher = (int)cipher;
+    
+    if (int_cipher >= 97 && int_cipher <= 99)
+    {
+        switch (cipher)
+       {
+        case 'a':
+            printf("Case a selected");  
+            break;
+            
+        case 'b':     
+            printf("Case b Selected");
+            break;
+        }
+    }
+    
+    else if (int_cipher < 97 || int_cipher > 99)
+    {
+        printf("Please make possible selection");
+        return 0;
+    }
+    
 }
--- Encryp_alg_mess_txt_key.c
+++ Encryp_alg_mess_txt_key.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+//definition of a function that perfoms encryption given an algorithm, message text, and key
+
 int main(void)
 {
-    printf("hello, world\n");
+    char cipher;
+    int int_cipher;
+    
+    // Menu that allows user to select type of cipher (NB Menu structure/workings is same as origional menu (ie in MAin.c)) 
+    printf("Select type of cipher: \n");
+    printf("    a: Rotation cipher\n");
+    printf("    b: Substitution cipher\n\n");
+    printf("Selection: "); 
+    scanf("%c", &cipher);
+    
+    int_cipher = (int)cipher;
+    
+    if (int_cipher >= 97 && int_cipher <= 99)
+    {
+        switch (cipher)
+       {
+        case 'a':
+            printf("Case a selected");  
+            break;
+            
+        case 'b':     
+            printf("Case b Selected");
+            break;
+        }
+    }
+    
+    else if (int_cipher < 97 || int_cipher > 99)
+    {
+        printf("Please make possible selection");
+        return 0;
+    }
+    
 }
