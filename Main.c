24ac42a0f2d435c9f33bfce55118aeacea77f434
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-09 06:26:51 +0000

    Function redefined to include: - rotation function for encryption - rotation function for decryption
    Rotation decryption code incorporated into Main.c

--- Main.c
+++ Main.c
@@ -4,7 +4,8 @@
 void Encryp_alg_mess_txt_key(char cipher); //prototype for a function that perfoms encryption given an algorithm, message text, and key
 void Decryp_alg_ciph_txt_key(char cipher); //prototype for a function that performs decryption given an algorithm, cipher text, and key
 void Decryp_ciph_txt_no_key(char cipher); // prototype for a function that performs decryption given cipher text and some assumptions of its contents without the key
-void Rotation_cipher(void);
+void Rotation_cipher_Encrypt(void);
+void Rotation_cipher_Decrypt(void);
 void Substitution_cipher(void);
 int main ()
 {
@@ -78,7 +79,7 @@ void Encryp_alg_mess_txt_key(char cipher)
        {
         case 'a':
             printf("Case a selected\n\n");  
-            Rotation_cipher();
+            Rotation_cipher_Encrypt();
             break;
             
         case 'b':     
@@ -99,6 +100,31 @@ void Encryp_alg_mess_txt_key(char cipher)
 void Decryp_alg_ciph_txt_key(char cipher)
 {
     printf ("Decryption with a key selected");
+    int int_cipher;
+    
+    int_cipher = (int)cipher;
+    
+    if (int_cipher >= 97 && int_cipher <= 99)
+    {
+        switch (cipher)
+       {
+        case 'a':
+            printf("Case a selected\n\n");  
+            Rotation_cipher_Decrypt();
+            break;
+            
+        case 'b':     
+            printf("Case b Selected\n\n");
+            Substitution_cipher();
+            break;
+        }
+    }
+    
+    else if (int_cipher < 97 || int_cipher > 99)
+    {
+        printf("Please make possible selection");
+        //return 0;
+    }
 
 } 
 
@@ -108,7 +134,7 @@ void Decryp_ciph_txt_no_key(char cipher)
     
 }
 
-void Rotation_cipher(void)
+void Rotation_cipher_Encrypt(void)
 {
    int k, i, I, letters[25], over, orig_letters[25], size = 100;
    
@@ -174,6 +200,80 @@ void Rotation_cipher(void)
         fprintf(output, "%s\n", encrypt_text);
 	}
 	
+}
+
+void Rotation_cipher_Decrypt(void)
+{
+    #include <stdio.h>
+#include <string.h>
+
+//Decryption:
+int main(void)
+{
+   int k, i, I, letters[25], under, orig_letters[25], size = 500;
+   char encrypt_letter, orig_letter, origional_text[size], encrypt_text[size];
+   FILE *input;
+   FILE *output;
+   
+   printf("Enter offset of rotation cipher: \n");
+   scanf("%d", &k);
+   
+   //ACII code range for capital letters is 65 - 90
+   
+   for (i = 0; i < 26; i++)
+   {
+       letters[i] = 65 + i; // assigns a ASCII code for the encrypt letter to a specifc location in an array
+       
+       orig_letters[i] =  65 - k + i; // assigns a ASCII code for the origional letter to a specifc location in an array
+        
+       if (orig_letters[i] < 65)
+       {
+           under = 65 - orig_letters[i]; // this allows the rotation flow back over to 65 (ie A) to account for the shift in the alphabet by k
+           orig_letters[i] = 91 - under;
+       }
+       
+       encrypt_letter = (char)letters[i]; //converts the ASCII code number to character 
+       orig_letter = (char)orig_letters[i]; //converts the ASCII code number to character 
+       
+       printf("%c now %c\n", orig_letter, encrypt_letter);       
+   }
+   
+   input = fopen("encrypted.txt", "r");
+	
+	
+	while(!feof(input))
+	{
+	     fgets(encrypt_text, size, (FILE *)input); //stores the chars in the text form the input file "origional.txt" into array origional_text 
+
+         size = (int)strlen(encrypt_text)+ 1;
+         
+	     printf("Old: %s", encrypt_text);
+        
+        //printf("Size = %d", size)
+        
+        for (I = 0; I < (size - 1); I++)
+        {
+             ////printf("%c", origional_text[4]);
+             for (i = 0; i < 26; i++)
+            {             
+             if (encrypt_text[I] == letters[i])
+             {
+                 origional_text[I] = orig_letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
+             }
+             if (origional_text[I] == ' ') 
+            {
+                 encrypt_text[I] = ' ';
+             }
+            }
+        }
+        
+        output = fopen("origional.txt", "w");
+        
+        printf("\nNew: %s ",origional_text);
+        fprintf(output, "%s\n", origional_text);
+	}
+  
+}
 }
 
 void Substitution_cipher(void)
