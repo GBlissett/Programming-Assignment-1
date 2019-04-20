910f8dae2598ef1b8e89c69894ba7a1f33ddf50a
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-20 13:43:40 +0000

    Function written for decryption without a key for rotation cipher which counts the frequency of letters and determines the most frequent. Since 'E' is the single most common letter, the rotation shift can be deduced and used to decrypt message.

--- Main.c
+++ Main.c
@@ -277,7 +277,73 @@ void Substitution_cipher_Decrypt(void)
 
 void Decryp_ciph_txt_no_key_Rotation(void)
 {
-     printf ("Rotation Decryption without a key selected");
+   int k, i, letters[alph], under, orig_letters[alph], size = 500, count, curr_cnt, freq_num,key, j;
+   char encrypt_letter, orig_letter, encrypt_text[size];
+   FILE *input;
+   
+  printf ("Rotation Decryption without a key selected\n\n");
+   
+  input = fopen("encrypted.txt", "r");
+	
+        while(!feof(input))
+	   {
+	     fgets(encrypt_text, size, (FILE *)input); //stores the chars in the text form the input file "origional.txt" into array origional_text 
+
+         size = (int)strlen(encrypt_text)+ 1;
+                
+        //printf("Size = %d", size)
+        
+        
+        count = 0, curr_cnt = 1, freq_num = 0, key = 0;
+        
+        for(i = 0; i < size - 1; i++)
+        {
+            key = encrypt_text[i];
+            
+            if ((key >= 65) && (key <= 90) && (key != freq_num))
+            {
+                for(j = i + 1; j < size; j++)
+                {
+                    if(key == encrypt_text[j])
+                    {
+                        curr_cnt++;
+                    }
+                }
+                
+                if(curr_cnt > count)
+                {
+                    freq_num = key;
+                    count = curr_cnt;
+                    curr_cnt = 1;
+                }
+            }               
+        }
+        printf("Num = %d and times = %d\n\n", freq_num, count);
+        
+        break;
+    }
+       
+        k = freq_num - 70;
+        
+       for (i = 0; i < 26; i++)
+       {
+           letters[i] = 65 + i; // assigns a ASCII code for the encrypt letter to a specifc location in an array
+           
+           orig_letters[i] =  65 - k + i; // assigns a ASCII code for the origional letter to a specifc location in an array
+            
+           if (orig_letters[i] < 65)
+           {
+               under = 65 - orig_letters[i]; // this allows the rotation flow back over to 65 (ie A) to account for the shift in the alphabet by k
+               orig_letters[i] = 91 - under;
+           }
+           
+           encrypt_letter = (char)letters[i]; //converts the ASCII code number to character 
+           orig_letter = (char)orig_letters[i]; //converts the ASCII code number to character 
+           
+           printf("%c now %c\n\n", orig_letter, encrypt_letter); 
+       }
+       
+      Decryption(orig_letters, letters);
     
 }
 
