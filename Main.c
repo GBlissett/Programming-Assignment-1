fec4ee73179535a249eaf38842adfa4c2e3fdc67
Gabriella Blissett <gabriella.blissett@uon.edu.au>
2019-04-20 12:11:22 +0000

    Main.c updated where two extra functions were added (ie an encryption function and a decryption function) which function (ie both substitution and rotation use). This shortens the length of my program and makes my program run faster (and more efficiently).

--- Main.c
+++ Main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+
+int alph = 26; 
 void Encryp_alg_mess_txt_key(char cipher); //prototype for a function that perfoms encryption given an algorithm, message text, and key
 void Decryp_alg_ciph_txt_key(char cipher); //prototype for a function that performs decryption given an algorithm, cipher text, and key
 void Decryp_ciph_txt_no_key(char cipher); // prototype for a function that performs decryption given cipher text and some assumptions of its contents without the key
@@ -8,13 +10,16 @@ void Rotation_cipher_Encrypt(void);
 void Rotation_cipher_Decrypt(void);
 void Substitution_cipher_Encrypt(void);
 void Substitution_cipher_Decrypt(void);
+void Encryption(int letters[alph], int orig_letters[alph]);
+void Decryption(int letters[alph], int orig_letters[alph]);
+
 int main ()
 {
     char selection, cipher;
     int int_selection;
     
     // Menu that allows user to select if they want to encrypt or decrypt (depending on the information they have)
-    printf("Select which Encryptioor Decryption to be performed: \n");
+    printf("Select which Encryptior Decryption to be performed: \n");
     printf("    A: Encryption given an algorithm, message text, and key\n");
     printf("    B: Decryption given an algorithm, cipher text, and key\n"); 
     printf("    C: Decryption given cipher text and some assumptions of its contents without the key\n\n");  
@@ -138,11 +143,9 @@ void Decryp_ciph_txt_no_key(char cipher)
 
 void Rotation_cipher_Encrypt(void)
 {
-   int k, i, I, letters[25], over, orig_letters[25], size = 100;
+   int k, i, letters[alph], over, orig_letters[alph];
+   char encrypt_letter, orig_letter;
    
-   char encrypt_letter, orig_letter, origional_text[size], encrypt_text[size];
-   FILE *input;
-   FILE *output;
    
    printf("Enter offset of rotation cipher: \n");
    scanf("%d", &k);
@@ -167,58 +170,13 @@ void Rotation_cipher_Encrypt(void)
        printf("%c now %c\n", orig_letter, encrypt_letter);       
    }
   
-	input = fopen("origional.txt", "r");
-	
-	
-	while(!feof(input))
-	{
-	     fgets(origional_text, size, (FILE *)input); //stores the chars in the text form the input file "origional.txt" into array origional_text 
-
-         size = (int)strlen(origional_text)+ 1;
-         
-	     printf("Old: %s", origional_text);
-        
-        //printf("Size = %d", size)
-        
-        for (I = 0; I < (size - 1); I++)
-        {
-             ////printf("%c", origional_text[4]);
-             if ((origional_text[I] >= 65) && (origional_text[I] <= 90))
-             {
-                 for (i = 0; i < 26; i++)
-                {             
-                     if (origional_text[I] == orig_letters[i])
-                     {
-                         encrypt_text[I] = letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
-                     }
-                }
-            }
-            if (origional_text[I] == ' ') 
-            {
-                 encrypt_text[I] = ' ';
-             }
-             if ((origional_text[I] >= 33) && (origional_text[I] <= 64))
-             {
-                 encrypt_text[I] = origional_text[I];
-             }
-        }
-        
-        output = fopen("encrypted.txt", "w");
-        
-        printf("\nNew: %s ",encrypt_text);
-        fprintf(output, "%s\n", encrypt_text);
-        break;
-	}
-	
-
+    Encryption(orig_letters, letters);
 }
 
 void Rotation_cipher_Decrypt(void)
 {
-   int k, i, I, letters[25], under, orig_letters[25], size = 500;
-   char encrypt_letter, orig_letter, origional_text[size], encrypt_text[size];
-   FILE *input;
-   FILE *output;
+   int k, i, letters[alph], under, orig_letters[alph];
+   char encrypt_letter, orig_letter;
    
    printf("Enter offset of rotation cipher: \n");
    scanf("%d", &k);
@@ -243,58 +201,14 @@ void Rotation_cipher_Decrypt(void)
        printf("%c now %c\n", orig_letter, encrypt_letter);       
    }
    
-   input = fopen("encrypted.txt", "r");
-	
-	while(!feof(input))
-	{
-	     fgets(encrypt_text, size, (FILE *)input); //stores the chars in the text form the input file "origional.txt" into array origional_text 
-
-         size = (int)strlen(encrypt_text)+ 1;
-         
-	     printf("Old: %s", encrypt_text);
-        
-        //printf("Size = %d", size)
-        
-        
-        for (I = 0; I < (size - 1); I++)
-        {
-             ////printf("%c", origional_text[4]);
-             if ((encrypt_text[I] >= 65) && (encrypt_text[I] <= 90))
-             {
-                 for (i = 0; i < 26; i++)
-                {             
-                     if (encrypt_text[I] == letters[i])
-                     {
-                         origional_text[I] = orig_letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
-                     }
-                }
-            }
-            if (encrypt_text[I] == ' ') 
-            {
-                 origional_text[I] = ' ';
-             }
-             if ((encrypt_text[I] >= 33) && (encrypt_text[I] <= 64))
-             {
-                 origional_text[I] = encrypt_text[I];
-             }
-        }
-        
-        
-        output = fopen("origional.txt", "w");
-        
-        printf("\nNew: %s ",origional_text);
-        fprintf(output, "%s\n", origional_text);
-        break;
-	}
+    Decryption(orig_letters, letters);
   
 }
 
 void Substitution_cipher_Encrypt(void)
 {
-   int i, I, orig_letters[25], letters[25], size = 500;
-   char encrypt_letter, orig_letter, origional_text[size], encrypt_text[size], ASCII;
-   FILE *input;
-   FILE *output;
+   int i, letters[alph], orig_letters[alph];
+   char encrypt_letter, orig_letter, ASCII;
    
     for (i = 0; i < 26; i++)
     {
@@ -317,7 +231,45 @@ void Substitution_cipher_Encrypt(void)
        printf("%c now %c\n", orig_letter, encrypt_letter);
    }
 
-     
+    Encryption(orig_letters, letters);
+}
+
+void Substitution_cipher_Decrypt(void)
+{
+   int i, letters[alph], orig_letters[alph];
+   char encrypt_letter, orig_letter, ASCII;
+   
+    for (i = 0; i < 26; i++)
+    {
+       orig_letters[i] =  65 + i; // assigns a ASCII code for the origional letter to a specifc location in an array
+    }
+
+   for (i = 0; i < 26; i++)
+   {
+       printf("Enter encrypted substitutaion value for %c: ", orig_letters[i]);
+       scanf("%c", &ASCII);
+       getchar();
+       
+       ASCII = (int)ASCII;
+       //printf("%d", ASCII);
+       letters[i] = ASCII;
+       
+       encrypt_letter = (char)letters[i]; //converts the ASCII code number to character 
+       orig_letter = (char)orig_letters[i]; //converts the ASCII code number to character 
+       
+       printf("%c now %c\n", orig_letter, encrypt_letter);
+   }
+   
+    Decryption(orig_letters, letters);
+}
+
+void Encryption(int letters[alph], int orig_letters[alph])
+{
+   int i, I, size = 500;
+   char origional_text[size], encrypt_text[size];
+   FILE *input;
+   FILE *output;
+
    input = fopen("origional.txt", "r");
 	
 	while(!feof(input))
@@ -360,37 +312,14 @@ void Substitution_cipher_Encrypt(void)
         break;
 	}
 }
-
-void Substitution_cipher_Decrypt(void)
+void Decryption(int letters[alph], int orig_letters[alph])
 {
-   int i, I, orig_letters[25], letters[25], size = 500;
-   char encrypt_letter, orig_letter, origional_text[size], encrypt_text[size], ASCII;
+   int i, I, size = 500;
+   char origional_text[size], encrypt_text[size];
    FILE *input;
    FILE *output;
-   
-    for (i = 0; i < 26; i++)
-    {
-       orig_letters[i] =  65 + i; // assigns a ASCII code for the origional letter to a specifc location in an array
-    }
 
-   for (i = 0; i < 26; i++)
-   {
-       printf("Enter encrypted substitutaion value for %c: ", orig_letters[i]);
-       scanf("%c", &ASCII);
-       getchar();
-       
-       ASCII = (int)ASCII;
-       //printf("%d", ASCII);
-       letters[i] = ASCII;
-       
-       encrypt_letter = (char)letters[i]; //converts the ASCII code number to character 
-       orig_letter = (char)orig_letters[i]; //converts the ASCII code number to character 
-       
-       printf("%c now %c\n", orig_letter, encrypt_letter);
-   }
-     
    input = fopen("encrypted.txt", "r");
-	
 	
 	while(!feof(input))
 	{
@@ -405,24 +334,30 @@ void Substitution_cipher_Decrypt(void)
         for (I = 0; I < (size - 1); I++)
         {
              ////printf("%c", origional_text[4]);
-             for (i = 0; i < 26; i++)
-            {             
-             if (encrypt_text[I] == letters[i])
+             if ((encrypt_text[I] >= 65) && (encrypt_text[I] <= 90))
              {
-                 origional_text[I] = orig_letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
-             }
-             if (origional_text[I] == ' ') 
-            {
-                 encrypt_text[I] = ' ';
-             }
+                 for (i = 0; i < 26; i++)
+                {             
+                     if (encrypt_text[I] == letters[i])
+                     {
+                         origional_text[I] = orig_letters[i]; //tests for match between a letter input and origional alphabet then assigns the new letter (as according to the above rotating cipher code)
+                     }
+                }
             }
+            if (encrypt_text[I] == ' ') 
+            {
+                 origional_text[I] = ' ';
+             }
+             if ((encrypt_text[I] >= 33) && (encrypt_text[I] <= 64))
+             {
+                 origional_text[I] = encrypt_text[I];
+             }
         }
         
         output = fopen("origional.txt", "w");
         
         printf("\nNew: %s ",origional_text);
         fprintf(output, "%s\n", origional_text);
-	}
-  
-}
+        break;
+    }    
 }
